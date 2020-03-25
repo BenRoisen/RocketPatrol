936b395aa0ae69ffc72c97821394f3ed87c4ea8d
Benjamin Roisen <broisen@ucsc.edu>
2020-03-24 21:58:41 -0700

    added 60-second game timer

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -62,16 +62,30 @@ class Play extends Phaser.Scene {
             fixedWidth: 100
         }
         this.scoreLeft = this.add.text(69, 54, this.p1Score, scoreConfig);
+
+        // game over flag
+        this.gameOver = false;
+        
+        // 60-second play clock
+        scoreConfig.fixedWidth = 0;
+        this.clock = this.time.delayedCall(60000, () => {
+            this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5);
+            this.add.text(game.config.width/2, game.config.height/2 + 64, '(F)ire to Restart', scoreConfig).setOrigin(0.5);
+            this.gameOver = true;
+        }, null, this);
     }
 
     update() {
         this.starfield.tilePositionX -= 4;
-        this.p1Rocket.update();
 
-        // update spaceships (x3)
-        this.ship01.update();
-        this.ship02.update();
-        this.ship03.update();
+        if(!this.gameOver) {
+            // update rocket sprite
+            this.p1Rocket.update();
+            // update spaceships (x3)
+            this.ship01.update();
+            this.ship02.update();
+            this.ship03.update();
+        }
         
         // check collisions
         if(this.checkCollision(this.p1Rocket, this.ship03)) {
