c9a2f340afabfaf84e8404fbbf1a35eae5cf2995
Benjamin Roisen <broisen@ucsc.edu>
2020-03-24 17:37:07 -0700

    Added background/borders to play scene

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 let config = {
     type: Phaser.CANVAS,
-        width: 640,
-        height: 480
+    width: 640,
+    height: 480,
+    scene: [ Menu, Play ]
 }
 
 let game = new Phaser.Game(config);
-console.log("I'M DIFFERENT NOW!");
